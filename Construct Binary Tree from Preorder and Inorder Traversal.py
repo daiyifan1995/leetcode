1e24a0df0f49e2e48a88c614ba9d3c6608822750
daiyifan1995 <daiyifan1995@foxmail.com>
2019-03-17 16:07:19 +0800

    Construct Binary Tree from Preorder and Inorder Traversal

---
+++ Construct Binary Tree from Preorder and Inorder Traversal.py
@@ -0,0 +1,55 @@
+import sys
+
+#利用前序和中序遍历确定一棵树
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def buildTree(self, preorder, inorder):
+
+        return self.tree(inorder, preorder)
+
+    def tree(self, inorder, preorder):#利用前序和中序遍历确定一棵树
+        if len(inorder) > 0:
+            root = TreeNode(preorder[0])
+            ind = inorder.index(root.val)
+            inorderleft = inorder[0:ind]
+            # if ind+1<len(inorder)-1:
+            inorderright = inorder[ind + 1:len(inorder)]
+            preorderleft = preorder[1:ind + 1]
+            preorderright = preorder[ind + 1:len(inorder)]
+            root.left = self.tree(inorderleft, preorderleft)
+            root.right = self.tree(inorderright, preorderright)
+            # else:
+            #     root =None
+        else:
+            root = None
+
+        return root
+
+    def traversalTree(self,root,tree):#树的前序遍历
+        if(root!=None):
+            tree.append(root.val)
+        if(root.left!=None):
+            self.traversalTree(root.left,tree)
+        if(root.right != None):
+            self.traversalTree(root.right,tree)
+        return tree
+
+if __name__ == "__main__":
+    # height = sys.stdin.readline().strip()
+    # height = height.split(" ")
+    # height = [int(x) for x in height]
+    preorder = [3,9,20,15,7]
+    inorder = [9,3,15,20,7]#input为preorder和inorder
+
+
+    NewSolution=Solution()
+    tree=[]
+    root=NewSolution.buildTree(preorder, inorder)
+    print(NewSolution.traversalTree(root,tree))
